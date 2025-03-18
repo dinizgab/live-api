1d7ad947b7f416a9a1dea763f0183134b6e8ff84
Gabriel Diniz <gabriel.diniz@inda.band>
2025-03-18 09:26:44 -0300

    .

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,79 +1,95 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
-	"github.com/gorilla/websocket"
+	"golang.org/x/net/websocket"
 )
-
-type Server struct {
-	conns map[*websocket.Conn]bool
-	Rooms map[string]*websocket.Conn
-}
-
-func NewServer() *Server {
-	return &Server{
-		Rooms: make(map[string]*websocket.Conn),
-	}
-}
-
-func (s *Server) HandleWS(w http.ResponseWriter, r *http.Request) {
-	upgrader := websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-	}
-
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Printf("Failed to upgrade websocket: %v", err)
-		return
-	}
-	defer conn.Close()
-
-	fmt.Printf("New incoming connections from: %s", conn.RemoteAddr())
-
-	s.conns[conn] = true
-
-	s.readLoop(conn)
-}
-
-func (s *Server) readLoop(conn *websocket.Conn) {
-	for {
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-			fmt.Printf("Error reading message connection: %s", err)
-			continue
-		}
-
-		fmt.Printf("Received message: %s", msg)
-	}
-}
-
-func (s *Server) JoinRoom(room string, conn *websocket.Conn) {
-	s.Rooms[room] = conn
-}
 
 type Message struct {
 	Type string `json:"type"`
 	Data string `json:"data"`
 }
 
-func handleJoin(conn *websocket.Conn, message Message) {
-	log.Println(conn)
-	log.Printf("User %s joined", message.Data)
+type Server struct {
+	conns map[*websocket.Conn]bool
+	mu sync.Mutex
+	//Rooms map[string][]*websocket.Conn
 }
+
+func NewServer() *Server {
+	return &Server{
+		//Rooms: make(map[string][]*websocket.Conn),
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) HandleWS(conn *websocket.Conn) {
+	log.Printf("New incoming connection from: %s", conn.RemoteAddr())
+	s.mu.Lock()
+	s.conns[conn] = true
+	s.mu.Unlock()
+
+	defer s.removeConnection(conn)
+
+	for {
+		var msg Message
+		err := websocket.JSON.Receive(conn, &msg)
+		if err != nil {
+			log.Printf("Error reading message connection: %s", err)
+			continue
+		}
+
+		log.Printf("Received WebRTC Signal: %s from %s", msg.Type, conn.RemoteAddr())
+		s.broadcast(msg, conn)
+	}
+}
+
+func (s *Server) broadcast(message Message, sender *websocket.Conn) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	for conn, _ := range s.conns {
+		if conn != sender {
+			go func(c *websocket.Conn) {
+				err := websocket.JSON.Send(c, message)
+				if err != nil {
+					log.Printf("Error broadcasting message: %v", err)
+					s.removeConnection(c)
+				}
+			}(conn)
+		}
+	}
+}
+
+func (s *Server) removeConnection(conn *websocket.Conn) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	delete(s.conns, conn)
+	conn.Close()
+	log.Printf("Connection closed: %s\n", conn.RemoteAddr())
+}
+
+//func (s *Server) joinRoom(room string, conn *websocket.Conn) {
+//s.mu.Lock()
+//defer s.mu.Unlock()
+//
+//if _, ok := s.Rooms[room]; !ok {
+//s.Rooms[room] = append(s.Rooms[room], conn)
+//}
+//
+//if _, ok := s.conns[conn]; !ok {
+//s.conns[conn] = room
+//}
+//}
 
 func main() {
 	server := NewServer()
 
-	http.Handle("/ws", http.HandlerFunc(server.HandleWS))
+	http.Handle("/ws", websocket.Handler(server.HandleWS))
 
 	log.Println("Server started!")
 	log.Fatal(http.ListenAndServe(":3000", nil))
